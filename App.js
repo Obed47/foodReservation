237a3fc30db95a5a6ff3b73baf79f0ea5580d60c
simbaProject <simbaecommerce@gmail.com>
2025-04-01 19:54:59 +0100

    added components on app component. todo : add navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { View } from "react-native";
 import Onboarding from "./screens/Onboarding";
 import Categories from "./screens/Categories";
+import { Dimensions } from "react-native";
 import Order from "./screens/Order";
 export default function App() {
   return (
     <View>
-      <Onboarding />
+      <Order />
     </View>
   );
 }
